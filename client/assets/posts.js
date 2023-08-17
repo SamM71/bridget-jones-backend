ff078f5993b50177be96a740c8cbdaff358dfcff
Robbie Stamp <stamprobbie@gmail.com>
2023-08-17 13:09:51 +0100

    adding createPost function to index.js

---
+++ client/assets/posts.js
@@ -0,0 +1,14 @@
+function createPost (data) {
+  const post = document.createElement("div");
+  post.className = "post";
+
+  const header = document.createElement("div");
+  header.textContent = data["title"];
+  post.appendChild(header);
+
+  const content = document.createElement("h2");
+  content.textContent = data["content"];
+  post.appendChild(content);
+
+  return post;
+}
